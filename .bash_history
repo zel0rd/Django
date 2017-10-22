e372b4b4842ed924f0820bbfa5242a4c3f241233
Kwoun younggi <zelord.kwoun@gmail.com>
2017-10-22 05:36:52 +0000

    My Django Girls app, first commit

---
+++ .bash_history
@@ -0,0 +1,3 @@
+ssh -p 222 root
+ssh root -p222
+ssh root@zel0rd -p 222
--- python/django_bookmarks/django_bookmarks/urls.py
+++ python/django_bookmarks/django_bookmarks/urls.py
@@ -1,4 +1,4 @@
-"""djaongo_bookmarks URL Configuration
+"""django_bookmarks URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,18 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-"""
 from django.conf.urls import url
 from django.contrib import admin
-"""
-from django.conf.urls import url, include
-from bookmarks.views import *
-from django.conf.urls import patterns
-"""
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
-"""
-urlpatterns = patterns('',
-    (r'^$',main_page),
-)
